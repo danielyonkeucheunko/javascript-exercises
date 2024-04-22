252a6725eebeefe4da31486d6bd0a9fe41454ce2
Daniel Yonkeu <daniel.yonkeucheunko@gmail.com>
2024-04-22 03:57:38 -0400

    Finished Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...removable) {
+    const newArr = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        if (!removable.includes(arr[i])) {
+            newArr.push(arr[i]);
+        }
+    }
+
+    return newArr;
 };
 
 // Do not edit below this line
