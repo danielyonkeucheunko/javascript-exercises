781c6c41237af9febac418f8661b159b260fb769
Daniel Yonkeu <daniel.yonkeucheunko@gmail.com>
2024-04-29 22:35:07 -0400

    Finished Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (celsius) {
+    return Math.round((celsius - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (fahrenheit) {
+    return Math.round((fahrenheit * 1.8 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit,
 };
