5b2d8552c96ce69ef87362ac762bd94b1ac8a3d4
Daniel Yonkeu <daniel.yonkeucheunko@gmail.com>
2024-05-01 21:38:07 -0400

    Finished Exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (arr) {
+    const books = arr.map((book) => {
+        return book.title;
+    });
+    return books;
 };
 
 // Do not edit below this line
