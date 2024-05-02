0137d03dc651dfac96fe9f40e5826045c653884a
Daniel Yonkeu <daniel.yonkeucheunko@gmail.com>
2024-05-01 21:25:40 -0400

    Finished Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num === "string") {
+        num = Number(num);
+    }
 
+    if (num < 0) {
+        return "OOPS";
+    }
+    let fib1 = 0;
+    let fib2 = 1;
+
+    while (num > 0) {
+        let temp = fib2;
+        fib2 += fib1;
+        fib1 = temp;
+        num--;
+    }
+    return fib1;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+fibonacci(8);
