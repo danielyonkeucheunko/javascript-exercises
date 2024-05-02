60cc41912a20f2813fac22f6b140e60ef7ff95e9
Daniel Yonkeu <daniel.yonkeucheunko@gmail.com>
2024-05-01 20:33:09 -0400

    Finished Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,49 @@
-const add = function() {
-	
+const add = function (a, b) {
+    return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+    return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    return total;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+    let total = 1;
+    for (let i = 0; i < array.length; i++) {
+        total *= array[i];
+    }
+    return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+    return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+    if (num === 0) {
+        return 1;
+    }
+    let factorial = 1;
+
+    for (; num != 0; num--) {
+        factorial *= num;
+    }
+    return factorial;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial,
 };
