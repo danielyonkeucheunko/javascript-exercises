f6c804268154cd811bb0a63c188b1ed9272a7cc4
Daniel Yonkeu <daniel.yonkeucheunko@gmail.com>
2024-05-01 21:52:03 -0400

    Finished Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+    const sortedPeople = arr.sort((a, b) => {
+        return (a.yearOfDeath | new Date()) - a.yearOfBirth <
+            (b.yearOfDeath | new Date()) - b.yearOfBirth
+            ? 1
+            : -1;
+    });
+    console.table(sortedPeople);
 
+    return sortedPeople[0];
 };
 
 // Do not edit below this line
